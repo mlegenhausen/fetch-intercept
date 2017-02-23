b7e36f932baeaa877e7b36320e5e027ff96deb3a
Malte Legenhausen <mlegenhausen@gmail.com>
2017-02-23 15:58:56 +0100

    Closed #9
    Revert "Remove irrelevant code"
    
    This reverts commit 14e3c2264a0134d6fe59659d6063afec64f72598.

--- src/index.js
+++ src/index.js
@@ -10,12 +10,14 @@ const ENVIRONMENT_IS_NODE = typeof process === 'object' && typeof require === 'f
 const ENVIRONMENT_IS_WEB = typeof window === 'object';
 const ENVIRONMENT_IS_WORKER = typeof importScripts === 'function';
 
-if (ENVIRONMENT_IS_REACT_NATIVE || ENVIRONMENT_IS_NODE) {
+if (ENVIRONMENT_IS_REACT_NATIVE) {
   attach(global);
 } else if (ENVIRONMENT_IS_WORKER) {
   attach(self);
 } else if (ENVIRONMENT_IS_WEB) {
   attach(window);
+} else if (ENVIRONMENT_IS_NODE) {
+  attach(global);
 } else {
   throw new Error('Unsupported environment for fetch-intercept');
 }

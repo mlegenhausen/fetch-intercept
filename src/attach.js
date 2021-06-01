6a069f0299447d695f5bdf4d8c8657fc073b1821
Aurélien LEMAITRE <chevlu@gmail.com>
2021-06-01 10:46:06 +0200

    Add safety call on request

--- src/attach.js
+++ src/attach.js
@@ -16,18 +16,16 @@ function interceptor(fetch, ...args) {
     }
   });
 
-  let request = null;
-
   // Register fetch call
   promise = promise.then(args => {
-    request = new Request(...args);
-    return fetch(request);
-  }).then(response => {
-    response.request = request;
-    return response;
-  }).catch(error => {
-    error.request = request;
-    return Promise.reject(error);
+    const request = new Request(...args);
+    return fetch(request).then(response => {
+      response.request = request;
+      return response;
+    }).catch(error => {
+      error.request = request;
+      return Promise.reject(error);
+    });
   });
 
   // Register response interceptors
